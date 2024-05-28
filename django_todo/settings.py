8e09291d1895b748277562c762d8f702101702f1
bauka <newnewernewest123@gmail.com>
2024-05-28 22:12:22 +0500

    Error loading psycopg2 or psycopg module

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+# import psycopg2_binary
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todolist',
+        'USER': 'nurbergen',
+        'PASSWORD': 'qwerty123',
+        'HOST': 'localhost',
     }
 }
 
--- task/models.py
+++ task/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    comment = models.TextField(max_length=150, blank=True)
+    created_time = models.DateField(auto_now_add=True)
+    is_done = models.BooleanField(default=False, blank=True)
+    photo = models.ImageField(blank=True, null=True)
+    deleted_time = models.DateField(auto_now_add=True)
